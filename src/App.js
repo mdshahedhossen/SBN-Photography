38a973f4e2314c46fd13d7bef5e0683ed8eec593
mdshaedhossen <shaheddiu23@gmail.com>
2022-04-18 16:52:10 +0600

    about

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ServicesDetails from './Pages/ServicesDetails/ServicesDetails';
 import ReqireAuth from './Pages/ReqireAuth/ReqireAuth';
 import Footer from './Pages/Shared/Footer/Footer';
 import Gallery from './Pages/Gallery/Gallery';
+import About from './Pages/About/About';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>        
         <Route path='/home' element={<Home></Home>}></Route> 
         <Route path='/gallery' element={<Gallery></Gallery>}></Route> 
+        <Route path='/about' element={<About></About>}></Route> 
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/service/:serviceid' element={
---
+++ src/Pages/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import personalPic from '../../Images/shahed.jpg'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className='about-container'>
+           <div className='personla-info'>
+               <img src={personalPic} alt="" />
+               <div>
+                   <h3>Md. Shahed Hossen</h3>
+                </div>
+           </div>
+           <div className='informaiton'>
+               <p>
+                   I Completed B.Sc in CSE from Daffodil International University. In future I will going to abroad for my higher education, that's why i am makig all my preparations. At this same time I am continue in my research activites. Even after all this, I am still continuing my Programming-hero course. I don't know how long I will able to continue like this. 
+               </p>
+           </div>
+        </div>
+    );
+};
+
+export default About;
